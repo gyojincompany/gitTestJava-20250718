9322443502588f8f70c20aa7640388d10e63aab8
gyojincompany <luckyred@gmail.com>
2025-07-18 09:14:50 +0900

    깃허브 연동 연습 소스 추가

---
+++ src/chapter03/Helloworld.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		System.out.print("Helloworld!!");
+		
+		byte math; //byte 정수 변수 선언
+		math = 10; //대입연산자로 math변수에 85를 저장(이진수 1010)
+//		85 = math;
+//		math <- 85;
+		short total;
+	}
+
+}
